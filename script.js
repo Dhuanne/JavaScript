1f7a40596986d0f5992158cf6af65c1aed7a50db
Dave14 <Dave14@gmail.com>
2022-10-25 21:56:04 +0900

    New Life

--- script.js
+++ script.js
@@ -1,90 +1,108 @@
-// Variables
-// var eliminationScore = 0;
-// var allScore = identificationScore + eliminationScore;
-
-// To get score
-
-function checkIdentification(identificationScore) {
-
-    //parsing json to an aobject
-    /*var jsonidentification = JSON.parse(ientification);
-    for(var i = 0; i<jsonidentification.identification.length; i++){
-        var identification = jsonidentification.identification[i];
-        console.log(identification.qustions);
-        console.log(identification.answers);
-    }*/
-
-    // Score Variable
-    var identificationScore = 0;
-
-    // Identification
-    var q1Identification = document.getElementById("q1").value;
-    var q2Identification = document.getElementById("q2").value;
-    var q3Identification = document.getElementById("q3").value;
-    var q4Identification = document.getElementById("q4").value;
-    var q5Identification = document.getElementById("q5").value;
-    var q6Identification = document.getElementById("q6").value;
-    var q7Identification = document.getElementById("q7").value;
-    var q8Identification = document.getElementById("q8").value;
-    var q9Identification = document.getElementById("q9").value;
-    var q10Identification = document.getElementById("q10").value;
-
-    // Checking Identification
-    if (q1Identification == "Prototype") {identificationScore++}
-    if (q2Identification == "Mockup") {identificationScore++}
-    if (q3Identification == "Evolutionary Prototyping") {identificationScore++}
-    if (q4Identification == "Design") {identificationScore++}
-    if (q5Identification == "Software Design") {identificationScore++}
-    if (q6Identification == "Architectural Design") {identificationScore++}
-    if (q7Identification == "Throwaway Prototyping") {identificationScore++}
-    if (q8Identification == "Repository") {identificationScore++}
-    if (q9Identification == "Client-server") {identificationScore++}
-    if (q10Identification == "Peer-to-peer") {identificationScore++}
-
-    document.write("Your score is: " + identificationScore);
-
-    location.href = "softwareEngingeeringElimination.html";
-    return identificationScore;
-}
-
-// function print() {
-//     document.write(identificationScore=checkIdentification());
-// }
-
-function check() {
-    // Score variable
-    var eliminationScore = 0;
-
-    // Elimination
-    var q1=document.softEngElimination.question1.value;
-    var q2=document.softEngElimination.question2.value;
-    var q3=document.softEngElimination.question3.value;
-    var q4=document.softEngElimination.question4.value;
-    var q5=document.softEngElimination.question5.value;
-    var q6=document.softEngElimination.question6.value;
-    var q7=document.softEngElimination.question7.value;
-    var q8=document.softEngElimination.question8.value;
-    var q9=document.softEngElimination.question9.value;
-    var q10=document.softEngElimination.question10.value;
-
-    // If true add score
-    if (q1 == "Use-based development") {eliminationScore++}
-    if (q2 == "Object-Based Development Model") {eliminationScore++}
-    if (q3 == "Software does “wear out”") {eliminationScore++}
-    if (q4 == "None of the above") {eliminationScore++}
-    if (q5 == "Last explicit process model") {eliminationScore++}
-    if (q6 == "None of the above") {eliminationScore++}
-    if (q7 == "Less effective than waterfall model") {eliminationScore++}
-    if (q8 == "Increase rework in the development process") {eliminationScore++}
-    if (q9 == "Delayed error elimination") {eliminationScore++}
-    if (q10 == "None of the above") {eliminationScore++}
-
-    document.write("Your score is: " + eliminationScore);
-}
-function allScore() {
-    var identificationScore = checkIdentification();
-    var allScore = identificationScore + eliminationScore;
-    // All score
-    document.write("Your score is: " + allScore);
-}
-document.addEventListener('contextmenu', event => event.preventDefault());
+var myQuestions = [
+    {
+      question: "What is 10/2?",
+      answers: {
+        a: '3',
+        b: '5',
+        c: '115'
+      },
+      correctAnswer: 'b'
+    },
+    {
+      question: "What is 30/3?",
+      answers: {
+        a: '3',
+        b: '5',
+        c: '10'
+      },
+      correctAnswer: 'c'
+    }
+  ];
+  
+  var quizContainer = document.getElementById('quiz');
+  var resultsContainer = document.getElementById('results');
+  var submitButton = document.getElementById('submit');
+  
+  generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+  
+  function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+  
+    function showQuestions(questions, quizContainer){
+      // we'll need a place to store the output and the answer choices
+      var output = [];
+      var answers;
+  
+      // for each question...
+      for(var i=0; i<questions.length; i++){
+        
+        // first reset the list of answers
+        answers = [];
+  
+        // for each available answer...
+        for(letter in questions[i].answers){
+  
+          // ...add an html radio button
+          answers.push(
+            '<label>'
+              + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+              + letter + ': '
+              + questions[i].answers[letter]
+            + '</label>'
+          );
+        }
+  
+        // add this question and its answers to the output
+        output.push(
+          '<div class="question">' + questions[i].question + '</div>'
+          + '<div class="answers">' + answers.join('') + '</div>'
+        );
+      }
+  
+      // finally combine our output list into one string of html and put it on the page
+      quizContainer.innerHTML = output.join('');
+    }
+  
+  
+    function showResults(questions, quizContainer, resultsContainer){
+      
+      // gather answer containers from our quiz
+      var answerContainers = quizContainer.querySelectorAll('.answers');
+      
+      // keep track of user's answers
+      var userAnswer = '';
+      var numCorrect = 0;
+      
+      // for each question...
+      for(var i=0; i<questions.length; i++){
+  
+        // find selected answer
+        userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+        
+        // if answer is correct
+        if(userAnswer===questions[i].correctAnswer){
+          // add to the number of correct answers
+          numCorrect++;
+          
+          // color the answers green
+          answerContainers[i].style.color = 'lightgreen';
+        }
+        // if answer is wrong or blank
+        else{
+          // color the answers red
+          answerContainers[i].style.color = 'red';
+        }
+      }
+  
+      // show number of correct answers out of total
+      resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+    }
+  
+    // show questions right away
+    showQuestions(questions, quizContainer);
+    
+    // on submit, show results
+    submitButton.onclick = function(){
+      showResults(questions, quizContainer, resultsContainer);
+    }
+  
+  }
