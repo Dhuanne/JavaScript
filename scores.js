25adfb73d64dbda1a9ff7b7b6d5527818dca24d4
Dave14 <Dave14@gmail.com>
2022-11-09 02:58:48 +0900

    JavaScript Update

---
+++ scores.js
@@ -0,0 +1,14 @@
+const highScoresList = document.getElementById("highScoresList");
+const score = JSON.parse(localStorage.getItem("scores")) || [];
+
+highScoresList.innerHTML = score
+  .map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  })
+  .join("");
+
+// Clears local Storage
+function clearLocalStorage() {
+    localStorage.clear();
+    location.reload();
+}
--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-
+// Validates Admin Login
 function teacherValidate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if (username == "admin" && password == "admin") {
-        alert("login succesfully");
-        return false;
+        window.location.href = "scores.html"
     } else {
         alert("login failed");
     }
-}
-function studentValidate() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    if (username.replaceAll(" ", "") == "" && password.replaceAll(" ", "") == "" || username.replaceAll(" ", "") == "" || password.replaceAll(" ", "") == "") {
-        alert("Fill out all the required fields");
-    } else{
-        alert("login succesfully");
-        window.location.href = "subject.html"
-        return false;
-    }
 
+}
+// Validates Student
+function studentValidate() {
+    window.location.href = "questions.html"
 }
