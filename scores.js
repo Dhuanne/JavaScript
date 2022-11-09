969f5b29e24c652ba20c5ebd54aebe2c3cf2c9e4
kenthu <2215422@slu.edu.ph>
2022-11-09 08:53:04 +0800

    Cleanup

--- scores.js
+++ scores.js
@@ -2,6 +2,7 @@ const highScoresList = document.getElementById("highScoresList");
 const score = JSON.parse(localStorage.getItem("scores")) || [];
 
 highScoresList.innerHTML = score
+  //maos the score odf the student
   .map(score => {
     return `<li class="high-score">${score.name} - ${score.score}/25</li>`;
   })
@@ -9,6 +10,6 @@ highScoresList.innerHTML = score
 
 // Clears local Storage
 function clearLocalStorage() {
-    localStorage.clear();
-    location.reload();
+    localStorage.clear(); //Clears the local storage 
+    location.reload();//refreshes the local storage
 }
